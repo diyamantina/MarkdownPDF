4c860115287661fd4a646e6f4bece7278925b4b8
Mihaela Mihaljevic <mihaelamj@me.com>
2026-07-01 01:52:14 +0200

    build(deps): move internal dependencies to Codeberg

--- Package.swift
+++ Package.swift
@@ -45,7 +45,7 @@ let package = Package(
     ] + macProducts,
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3"),
-        .package(url: "https://github.com/mihaelamj/MathTypeset.git", from: "0.6.0"),
+        .package(url: "https://codeberg.org/Mihaela/MathTypeset.git", from: "0.6.0"),
     ],
     targets: [
         .target(
